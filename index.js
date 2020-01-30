b9831c04fa22562b993ce158fdc9a4c31cc7a0c9
Praveen <kodadala.praveen@gmail.com>
2020-01-30 22:52:10 +0530

    Feature Extractor for Image Classification

--- index.js
+++ index.js
@@ -1,170 +1,109 @@
-
-//simple linear regression.
-
-// async function learnLinear(){
-    
-//     const model= tf.sequential();
-//     model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-//     model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-//     const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
-//     const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
-
-//     await model.fit(xs, ys, {epochs: 250});
-//     document.getElementById('output_field').innerText = 
-//     model.predict(tf.tensor2D([20], [1, 1])).print();
-    
-// }
-// learnLinear();
- 
-
-// below code is working up to 35 line
-
-// Define a model for linear regression.
-// const model = tf.sequential();
-// model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// // Generate some synthetic data for training.
-// const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-// const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-// // Train the model using the data.
-// model.fit(xs, ys, {epochs: 500}).then(() => {
-//   // Use the model to do inference on a data point the model hasn't seen before:
-//   document.getElementById('output_field').innerText = 
-//   model.predict(tf.tensor2d([5], [1, 1]));
-//   // Open the browser devtools to see the output
-// });
-
-
-
-// import * as tf from '@tensorflow/tfjs';
-
-// // Define a model for linear regression.
-// const model = tf.sequential();
-// model.add(tf.layers.dense({units: 1, inputShape: [1]}));
-
-// model.compile({loss: 'meanSquaredError', optimizer: 'sgd'});
-
-// // Generate some synthetic data for training.
-// const xs = tf.tensor2d([1, 2, 3, 4], [4, 1]);
-// const ys = tf.tensor2d([1, 3, 5, 7], [4, 1]);
-
-// // Train the model using the data.
-// model.fit(xs, ys, {epochs:500}).then(() => {
-//   // Use the model to do inference on a data point the model hasn't seen before:
-//   document.getElementById('output_field').innerText = 
-//   model.predict(tf.tensor2d([5], [1, 1])).print();
-//   // Open the browser devtools to see the output
-// });
-
-
-
-//testing code
-// tf.tensor5d([1, 2, 3, 4, 5, 6, 7, 8], [1, 2, 2, 2, 1]).print();
-
-
-//testing code
-// function setup(){
-     
-//     const values = [];
-//     for(let i=0; i < 30; i++){
-//         values[i] = math.random(0, 100);
-//     };
-    
-//     console.log(values);
-//     const shape = [2, 5, 3];
-//     const tense = tf.tensor3d(values, shape, 'int32');
-//     // console.log(data.toString());
-//     // console.log(data);
-//     tense.data().then(function(stuff){
-//         console.log(stuff)
-//     })
-//     };  
-//     setup();
-
-
-
-
-
-
-//below code is working with p5.js library
-
-let mobilenet;
+let featureExtractor;
 let classifier;
 let video;
-let label = 'loading model';
-let happyButton;
-let sadButton;
-let trainButton;
-
-function modelReady() {
-  console.log('Model is ready!!!');
-  // classifier.load('model.json', customModelReady);
-}
-
-// function customModelReady() {
-//   console.log('Custom Model is ready!!!');
-//   label = 'model ready';
-//   classifier.classify(gotResults);
-// }
-
-function videoReady() {
-  console.log('Video is ready!!!');
-}
+let loss;
+let dogImages = 0;
+let catImages = 0;
+let badgerImages = 0;
 
 function setup() {
-  createCanvas(320, 270);
+  noCanvas();
+  // Create a video element
   video = createCapture(VIDEO);
-  video.hide();
-  background(0);
-  mobilenet = ml5.featureExtractor('MobileNet', modelReady);
-  classifier = mobilenet.classification(video, videoReady);
+  video.parent('videoContainer');
+  video.size(320, 240);
 
-  happyButton = createButton('happy');
-  happyButton.mousePressed(function() {
-    classifier.addImage('happy');
+  // Extract the already learned features from MobileNet
+  featureExtractor = ml5.featureExtractor('MobileNet', modelReady);
+
+  // Create a new classifier using those features and give the video we want to use
+  const options = { numLabels: 3 };
+  classifier = featureExtractor.classification(video, options);
+  // Set up the UI buttons
+  setupButtons();
+}
+
+// A function to be called when the model has been loaded
+function modelReady() {
+  select('#modelStatus').html('MobileNet Loaded!');
+  // If you want to load a pre-trained model at the start
+  // classifier.load('./model/model.json', function() {
+  //   select('#modelStatus').html('Custom Model Loaded!');
+  // });
+}
+
+// Classify the current frame.
+function classify() {
+  classifier.classify(gotResults);
+}
+
+// A util function to create UI buttons
+function setupButtons() {
+  // When the Cat button is pressed, add the current frame
+  // from the video with a label of "cat" to the classifier
+  buttonA = select('#catButton');
+  buttonA.mousePressed(function() {
+    classifier.addImage('cat');
+    select('#amountOfCatImages').html(catImages++);
   });
 
-  sadButton = createButton('sad');
-  sadButton.mousePressed(function() {
-    classifier.addImage('sad');
+  // When the Dog button is pressed, add the current frame
+  // from the video with a label of "dog" to the classifier
+  buttonB = select('#dogButton');
+  buttonB.mousePressed(function() {
+    classifier.addImage('dog');
+    select('#amountOfDogImages').html(dogImages++);
   });
 
-  trainButton = createButton('train');
-  trainButton.mousePressed(function() {
-    classifier.train(whileTraining);
+  // When the Dog button is pressed, add the current frame
+  // from the video with a label of "dog" to the classifier
+  buttonC = select('#badgerButton');
+  buttonC.mousePressed(function() {
+    classifier.addImage('badger');
+    select('#amountOfBadgerImages').html(badgerImages++);
   });
 
-  saveButton = createButton('save');
-  saveButton.mousePressed(function() {
+  // Train Button
+  train = select('#train');
+  train.mousePressed(function() {
+    classifier.train(function(lossValue) {
+      if (lossValue) {
+        loss = lossValue;
+        select('#loss').html('Loss: ' + loss);
+      } else {
+        select('#loss').html('Done Training! Final Loss: ' + loss);
+      }
+    });
+  });
+
+  // Predict Button
+  buttonPredict = select('#buttonPredict');
+  buttonPredict.mousePressed(classify);
+
+  // Save model
+  saveBtn = select('#save');
+  saveBtn.mousePressed(function() {
     classifier.save();
   });
+
+  // Load model
+  loadBtn = select('#load');
+  loadBtn.changed(function() {
+    classifier.load(loadBtn.elt.files, function() {
+      select('#modelStatus').html('Custom Model Loaded!');
+    });
+  });
 }
 
-function draw() {
-  background(0);
-  image(video, 0, 0, 320, 240);
-  fill(255);
-  textSize(16);
-  text(label, 10, height - 10);
-}
-
-function whileTraining(loss) {
-  if (loss == null) {
-    console.log('Training Complete');
-    classifier.classify(gotResults);
-  } else {
-    console.log(loss);
+// Show the results
+function gotResults(err, results) {
+  // Display any error
+  if (err) {
+    console.error(err);
   }
-}
-
-function gotResults(error, result) {
-  if (error) {
-    console.error(error);
-  } else {
-    label = result;
-    classifier.classify(gotResults);
+  if (results && results[0]) {
+    select('#result').html(results[0].label);
+    select('#confidence').html(results[0].confidence.toFixed(2) * 100 + '%');
+    classify();
   }
 }
